431671ec643280ee6e0ed59ad1f32a800a16d37f
Soukaina-Ahlach <soukaina.ahlach.2001@gmail.com>
2024-05-25 17:09:54 +0200

    Updated main class is main.java

--- src/main/java/boardgame/main.java
+++ src/main/java/boardgame/main.java
@@ -1,4 +1,13 @@
 package boardgame;
 
+import javafx.application.Application;
+import boardgame.view.BoardGameApplication;
 public class main {
+
+
+    public static void main(String[] args) {
+        Application.launch(BoardGameApplication.class, args);
+
+
+    }
 }
