9bce038d1888aae0e5449684357771dce512fbef
Soukaina-Ahlach <soukaina.ahlach.2001@gmail.com>
2024-05-25 15:48:45 +0200

    Added the board initialization in StoneGameBoard.java

--- src/main/java/boardgame/model/StoneGameBoard.java
+++ src/main/java/boardgame/model/StoneGameBoard.java
@@ -8,5 +8,29 @@ import game.State;
 
 
 
-public class StoneGameBoard {
+public class StoneGameBoard implements TwoPhaseMoveState<Position> {
+    public static final int BOARD_SIZE = 4;
+    public final BooleanProperty[][] board;
+
+    private State.Player player;
+    public StoneGameBoard() {
+        board = new BooleanProperty[BOARD_SIZE][BOARD_SIZE];
+        // Initially, all cells contain stones
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                board[i][j] = new ReadOnlyBooleanWrapper(true);
+            }
+        }
+        player = State.Player.PLAYER_1;
+
+    }
+
+
+
+
+
+
+
+
 }
+
