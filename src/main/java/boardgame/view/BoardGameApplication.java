34d7af97c0482092895737a12790239729f61fa2
Soukaina-Ahlach <soukaina.ahlach.2001@gmail.com>
2024-05-25 17:12:06 +0200

    Added JavaFX application for board game UI in BoardGameApplication.java

--- src/main/java/boardgame/view/BoardGameApplication.java
+++ src/main/java/boardgame/view/BoardGameApplication.java
@@ -1,4 +1,21 @@
 package boardgame.view;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class BoardGameApplication {
+import java.io.IOException;
+public class BoardGameApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage.setTitle("Stone Board Game");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+
 }
