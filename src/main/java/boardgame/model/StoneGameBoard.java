20ebc502dd7757bd99b224b250b7235d94e7fbe9
Soukaina-Ahlach <soukaina.ahlach.2001@gmail.com>
2024-05-25 15:45:03 +0200

    Added import statements in StoneGameBoard.java

--- src/main/java/boardgame/model/StoneGameBoard.java
+++ src/main/java/boardgame/model/StoneGameBoard.java
@@ -1,4 +1,12 @@
 package boardgame.model;
 
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import game.TwoPhaseMoveState;
+import game.State;
+
+
+
 public class StoneGameBoard {
 }
