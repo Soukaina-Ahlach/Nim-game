1c94560e222fa2b1eda8488a6e398edc05b01536
Soukaina-Ahlach <soukaina.ahlach.2001@gmail.com>
2024-05-26 09:48:15 +0200

    Added the main class and main method API documentations in main.java

--- src/main/java/boardgame/main.java
+++ src/main/java/boardgame/main.java
@@ -2,9 +2,22 @@ package boardgame;
 
 import javafx.application.Application;
 import boardgame.view.BoardGameApplication;
+
+/**
+ * Entry point for the board game application.
+ * <p>
+ * This class contains the main method, which serves as the entry point for the application.
+ * It launches the JavaFX application by invoking the {@link javafx.application.Application#launch(Class, String[])}
+ * method with the {@link boardgame.view.BoardGameApplication} class.
+ * </p>
+ */
 public class main {
 
-
+    /**
+     * The main method to start the board game application.
+     *
+     * @param args Command-line arguments (not used in this application)
+     */
     public static void main(String[] args) {
         Application.launch(BoardGameApplication.class, args);
 
